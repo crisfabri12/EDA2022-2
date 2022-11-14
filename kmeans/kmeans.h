9dd71f0ade458bef55b6e928f302d9ab7cbd0be5
crisfabri12 <cristhian.ocola@ucsp.edu.pe>
2022-11-14 02:26:10 -0500

    Add files via upload

---
+++ kmeans/kmeans.h
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include<iostream>
+using namespace std;
+
+//K. MEANS CLUSTERING ALGORITHM
+
+struct Point
+{
+	int x;
+	int y;
+	int cluster;
+};
+
+struct Cluster
+{
+	int x;
+	int y;
+	int size;
+};
+
+void kmeans(vector<Point>& points, vector<Cluster>& clusters, int k,int iterations,vector<vector<Point>> &res)
+{
+	vector<vector<Point>> aux(k);
+	//initialize clusters
+	for (int i = 0; i < k; i++)
+	{
+		clusters[i].x = points[i].x;
+		clusters[i].y = points[i].y;
+		clusters[i].size = 1;
+	}
+	for (int i = 0; i < iterations; i++)
+	{
+		//assign points to clusters
+		for (int j = 0; j < points.size(); j++)
+		{
+			int min = 999999;
+			int cluster = 0;
+			for (int l = 0; l < k; l++)
+			{
+				int distance = (points[j].x - clusters[l].x) * (points[j].x - clusters[l].x) + (points[j].y - clusters[l].y) * (points[j].y - clusters[l].y);
+				if (distance < min)
+				{
+					min = distance;
+					cluster = l;
+				}
+			}
+			points[j].cluster = cluster;
+		}
+		
+		//recalculate clusters
+		for (int j = 0; j < k; j++)
+		{
+			clusters[j].x = 0;
+			clusters[j].y = 0;
+			clusters[j].size = 0;
+			aux[j].clear();
+		}
+		for (int j = 0; j < points.size(); j++)
+		{
+			clusters[points[j].cluster].x += points[j].x;
+			clusters[points[j].cluster].y += points[j].y;
+			clusters[points[j].cluster].size++;
+			aux[points[j].cluster].push_back(points[j]);
+		}
+		for (int j = 0; j < k; j++)
+		{
+			clusters[j].x /= clusters[j].size;
+			clusters[j].y /= clusters[j].size;
+		}
+	}
+	res = aux;
+}
---
+++ kmeans/main.cpp
@@ -0,0 +1,74 @@
+#include<GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include "kmeans.h"
+
+using namespace std;
+
+vector<vector<Point>> res;
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+	
+	glBegin(GL_POINTS);
+	
+	for(int i = 0; i < res.size();i++){
+		float randomR = (double)rand() / (RAND_MAX);
+		float randomG = (double)rand() / (RAND_MAX);
+		float randomB = (double)rand() / (RAND_MAX);
+		glColor3f(randomR, randomG, randomB);
+		for(int j = 0; j < res[i].size();j++){
+			glVertex2f(res[i][j].x, res[i][j].y);
+		}
+	}
+	glEnd();
+	glFlush();
+}
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+	gluOrtho2D(0.0, 100.0, 0.0,100.0);
+}
+
+int main(int argc, char** argv) {
+	
+	int n;
+	int k;
+	cout<<"Ingrese el numero de púntos: ";
+	cin>>n;
+	cout<<endl;
+	cout<<"Ingrese el numero de clusters: ";
+	cin>>k;
+
+	vector<Point> points(n);
+	for(int i = 0 ; i < n; i++){
+		points[i].x = rand()%100;
+		points[i].y = rand()%100;	
+	}
+	
+	//k-means clustering
+	vector<Cluster>clusters(k);
+	kmeans(points, clusters, k, 10,res);
+	
+
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
+	
+	myinit();
+	glutMainLoop();
+	
+	return 0;
+}
